711212984fb93eb0fe20ad88bfa4a25483de52df
aboutwout <woutervervloet@hoppinger.com>
2017-08-04 12:02:37 +0200

    Update formula

--- wkhtmltopdf0123.rb
+++ wkhtmltopdf0123.rb
@@ -1,25 +1,44 @@
-cask 'wkhtmltopdf' do
-  version '0.12.3'
-  sha256 '402209589279e092c94d17c76e6fdda6be5cadb21ce12e7c093c41f82b757506'
+require 'formula'
 
-  url "http://downloads.wkhtmltopdf.org/#{version.major_minor}/#{version}/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
-  name 'wkhtmltopdf'
+class Wkhtmltopdf < Formula
   homepage 'https://wkhtmltopdf.org/'
+  url 'http://downloads.wkhtmltopdf.org/#{version.major_minor}/#{version}/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg'
+  sha1 '402209589279e092c94d17c76e6fdda6be5cadb21ce12e7c093c41f82b757506'
+  version '0.12.3'
 
-  pkg "wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
+  depends_on 'qt'
 
-  uninstall pkgutil: 'org.wkhtmltopdf.wkhtmltox',
-            delete:  [
-                       '/usr/local/include/wkhtmltox',
-                       '/usr/local/lib/libwkhtmltox.dylib',
-                       "/usr/local/lib/libwkhtmltox.#{version.major}.dylib",
-                       "/usr/local/lib/libwkhtmltox.#{version.major_minor}.dylib",
-                       "/usr/local/lib/libwkhtmltox.#{version.sub(%r{-.*$}, '')}.dylib",
-                       '/usr/local/bin/wkhtmltoimage',
-                       '/usr/local/bin/wkhtmltopdf',
-                     ]
+  def install
+    # fix that missing TEMP= include.
+    inreplace 'common.pri' do |s|
+      s.gsub! 'TEMP = $$[QT_INSTALL_LIBS] libQtGui.prl', ''
+      s.gsub! 'include($$join(TEMP, "/"))', ''
+    end
 
-  caveats do
-    files_in_usr_local
+    # It tries to build universally, but Qt is bottled as 64bit => build error.
+    # If we are 64bit, do not compile with -arch i386.  This is a Homebrew
+    # issue with our Qt4, not upstream, because wkhtmltopdf bundles a patched
+    # Qt4 that Homebrew doesn't use.
+    if MacOS.prefer_64_bit?
+      inreplace 'src/pdf/pdf.pro', 'x86', Hardware::CPU.arch_64_bit
+      inreplace 'src/image/image.pro', 'x86', Hardware::CPU.arch_64_bit
+    end
+
+    if MacOS.version >= :mavericks && ENV.compiler == :clang
+      spec = 'unsupported/macx-clang-libc++'
+    else
+      spec = 'macx-g++'
+    end
+
+    system 'qmake', '-spec', spec
+    system 'make'
+    ENV['DYLD_LIBRARY_PATH'] = './bin'
+    `bin/wkhtmltopdf --manpage > wkhtmltopdf.1`
+    `bin/wkhtmltoimage --manpage > wkhtmltoimage.1`
+
+    # install binaries, libs, and man pages
+    bin.install Dir[ "bin/wkh*" ]
+    lib.install Dir[ "bin/lib*" ]
+    man1.install Dir[ "wkht*.1" ]
   end
 end
